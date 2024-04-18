1542db0b96f8d94c0c7dd6fbb3e2b419122fefb9
BeyonderCryptoOfi <alfonso.segoviam@gmail.com>
2024-04-18 16:09:14 -0600

    Componente de EventItems

--- src/components/Events/EventItem/EventItem.jsx
+++ src/components/Events/EventItem/EventItem.jsx
@@ -1,6 +1,10 @@
-const EventItem = () => {
+const EventItem = ({info, name, image}) => {
   return (
-    <div>EventItem</div>
+    <div>
+      <img src={image} alt={name} width={200} height={200}/>
+      <h4>{name}</h4>
+      <p>{info}</p>
+    </div>
   )
 }
 
--- src/components/Events/Events.jsx
+++ src/components/Events/Events.jsx
@@ -1,9 +1,21 @@
 import EventItem from "./EventItem/EventItem"
+import data from '../../data/events.json'
+
+const events = data._embedded.events;
+console.log(data);
 const Events = () => {
+    const eventsComponent = events.map((eventItem) =>(
+    <EventItem 
+    key={eventItem.id}
+    name={eventItem.name}    
+    info={eventItem.info}
+    image={eventItem.images[0].url}
+    /> 
+    ));
   return (
     <>
     <div>Events</div>
-    <EventItem />
+    {eventsComponent}
     </>
   )
 }
